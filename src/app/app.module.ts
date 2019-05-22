f2cf0072031dd7b5b1a873b8e65872c26edfecc3
MICHAELSWONG <mizik_jah@yahoo.com>
2019-05-21 22:45:59 -0400

    added shopping list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
@@ -23,7 +23,7 @@ import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-ed
     RecipeListComponent,
     ShoppingEditComponent
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
